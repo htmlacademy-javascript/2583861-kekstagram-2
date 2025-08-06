fdb9d628a7a916cd00c0e04df3920022322ca9e2
AlexanderShuvatov <shuvatovalexander@yandex.ru>
2025-08-06 19:20:47 +0400

    creates and adds js modules

---
+++ js/utils.js
@@ -0,0 +1,23 @@
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function createIdGenerator (min, max) {
+  const usedId = new Set();
+  return function () {
+    let currentId = getRandomNumber(min, max);
+    if (usedId.length >= max - min + 1) {
+      return null;
+    }
+    while (usedId.has(currentId)) {
+      currentId = getRandomNumber(min, max);
+    }
+    usedId.add(currentId);
+    return currentId;
+  };
+}
+
+export {getRandomNumber, createIdGenerator};
